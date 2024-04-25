036aa2b33f37bf4ba6a0bdea9238ff7c3bd3220a
Thales Brasileiro <thalesxbrasileiro@gmail.com>
2024-04-25 15:10:20 -0300

    feat: Criando o projeto com o header e o footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'path';
+import { ContatoComponent } from './page/contato/contato.component';
+import { SobreComponent } from './page/sobre/sobre.component';
+import { NaoEncontradoComponent } from './page/nao-encontrado/nao-encontrado.component';
+import { HomeComponent } from './page/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: '**', component: NaoEncontradoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,39 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { ContatoComponent } from './page/contato/contato.component';
+import { SobreComponent } from './page/sobre/sobre.component';
+import { RouterLink } from '@angular/router';
+import { NaoEncontradoComponent } from './page/nao-encontrado/nao-encontrado.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContatoComponent,
+    SobreComponent,
+    NaoEncontradoComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
